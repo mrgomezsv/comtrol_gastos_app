5f4bd4748280ddd803c172ae7951c7f58492dbfa
Mario Roberto <mrgomez.dev@gmail.com>
2024-03-08 15:44:03 -0600

    create Composable ExpensesTotalHeader

---
+++ composeApp/src/androidMain/kotlin/previews/ProjectPReviews.kt
@@ -0,0 +1,17 @@
+package previews
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ui.ExpensesTotalHeader
+
+@Preview(showBackground = true)
+@Composable
+fun ExpensesTotalHeaderPreview() {
+    Box(modifier = Modifier.padding(16.dp)) {
+        ExpensesTotalHeader(total = 1028.8)
+    }
+}
--- composeApp/src/commonMain/kotlin/ui/ExpensesScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/ExpensesScreen.kt
@@ -4,12 +4,21 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import getColorsTheme
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -29,6 +38,28 @@ fun ExpensesScreen() {
         }
         items(emptyList<String>()) {
             // Composables
+        }
+    }
+}
+
+@Composable
+fun ExpensesTotalHeader(total: Double) {
+    Card(shape = RoundedCornerShape(30), backgroundColor = Color.Black, elevation = 5.dp) {
+        Column(
+            modifier = Modifier.fillMaxWidth().height(130.dp).padding(16.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "$$total",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.White
+            )
+            Text(
+                modifier = Modifier.align(Alignment.End),
+                text = "USD",
+                color = Color.Gray
+            )
         }
     }
 }
