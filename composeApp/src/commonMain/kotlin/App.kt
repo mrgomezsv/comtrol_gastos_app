90c719f59e4e554800775cbebcf97629874878d5
Mario Roberto <mrgomez.dev@gmail.com>
2024-03-08 14:05:25 -0600

    first commit

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,18 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import moe.tlaster.precompose.PreComposeApp
+import moe.tlaster.precompose.viewmodel.ViewModel
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+
+@Composable
+fun App() {
+    PreComposeApp {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Text("Bienvenidos")
+            Text("Curso Kotlin Multiplataforma con Compose")
+        }
+    }
+}
